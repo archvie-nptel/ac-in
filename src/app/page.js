609a50804665684dee2865416e08dbf72bffef5d
archvie-nptel <mrr712005@gmail.com>
2024-10-23 01:29:41 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,5 @@
-export default function Page() {
-    return (
-      <div style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
-        <a
-         // href="/archive.nptel.ac.in/content/noc/NOC24/SEM1/Ecertificates/107/noc24-cs99/Course"
-         //href ="/certificate.pdf"
-         href ="../../content/noc/NOC24/SEM2/Ecertificates/107/noc24-cs71/Course/NPTEL24CS71S25010093502619814.pdf"
-          target="_blank"
-          style={{
-            color: 'white',
-            backgroundColor: '#3070bf',
-            padding: '10px 20px',
-            borderRadius: '8px',
-            textDecoration: 'none',
-            display: 'inline-block',
-            width: '156px',
-          }}
-        >
-          Course Certificate
-        </a>
-        <br />
-      </div>
-    );
-  }
-  
+import { redirect } from 'next/navigation';
+
+export default function HomePage() {
+  redirect('https://nptel.ac.in/');
+}
