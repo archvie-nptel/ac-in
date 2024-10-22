7642b8c2a198b8f579ef07bc83dc7b81b73f04d1
archvie-nptel <mrr712005@gmail.com>
2024-10-23 01:22:13 +0530

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,25 @@
+export default function Page() {
+    return (
+      <div style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
+        <a
+         // href="/archive.nptel.ac.in/content/noc/NOC24/SEM1/Ecertificates/107/noc24-cs99/Course"
+         //href ="/certificate.pdf"
+         href ="../../content/noc/NOC24/SEM2/Ecertificates/107/noc24-cs71/Course/NPTEL24CS71S25010093502619814.pdf"
+          target="_blank"
+          style={{
+            color: 'white',
+            backgroundColor: '#3070bf',
+            padding: '10px 20px',
+            borderRadius: '8px',
+            textDecoration: 'none',
+            display: 'inline-block',
+            width: '156px',
+          }}
+        >
+          Course Certificate
+        </a>
+        <br />
+      </div>
+    );
+  }
+  
